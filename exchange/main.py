e5a1fb0c60314fa11a70b0e79a2810f598a75da6
Boris R <borisrozhkin@gmail.com>
2023-05-09 18:15:49 +0500

    Create a loop to check exchange stocks depth

--- exchange/main.py
+++ exchange/main.py
@@ -1,9 +1,12 @@
+import time
+
 from poloniex import EPoloniex
 from yobit import EYobit
 from lbank import  ELbank
 from blockchain import EBlockchain
 from binance import EBinance
-def main():
+
+def periodic():
     ey = EYobit()
     ey.get_bigask(coin1='btc')
 #    ey.get_bigask(coin1='eth')
@@ -24,11 +27,21 @@ def main():
     bi.get_bigask(coin1='btc')
 #    bi.get_bigask(coin1='eth')
 
-    print(ey.get_url(), ey.bidask)
-    print(po.get_url(), po.bidask)
-    print(lb.get_url(), lb.bidask)
-    print(bc.get_url(), bc.bidask)
-    print(bi.get_url(), bi.bidask)
+#    print(ey.get_url(), ey.bidask)
+#    print(po.get_url(), po.bidask)
+#    print(lb.get_url(), lb.bidask)
+#    print(bc.get_url(), bc.bidask)
+#    print(bi.get_url(), bi.bidask)
+    lds = list()
+    lds.append(ey)
+    lds.append(po)
+    lds.append(lb)
+    lds.append(bc)
+
+    for l in lds:
+        print( bi.get_url(), ' : ', l.get_url(), " : ","bigs", round(100*(float(l.bidask['btc_usdt']['bids'][0][0])-float(bi.bidask['btc_usdt']['bids'][0][0]))/float(bi.bidask['btc_usdt']['bids'][0][0]), 2), "asks", round(100*(float(l.bidask['btc_usdt']['asks'][0][0])-float(bi.bidask['btc_usdt']['asks'][0][0]))/float(bi.bidask['btc_usdt']['asks'][0][0]), 2) )
 
 if __name__=='__main__':
-    main()
+    while(True):
+        periodic()
+        time.sleep(1)
